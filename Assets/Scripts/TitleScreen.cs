7d4a32f600f0596529a892a1a03f8d2d568fa302
Rainsing Tsai <rainsing.tsai@gmail.com>
2015-07-11 23:23:27 +0800

    Added title screen.

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour 
+{
+	public Image bgImage;
+	public Sprite introTexture;
+
+	private enum MyScreen
+	{
+		SCREEN_TITLE,
+		SCREEN_INTRO
+	}
+
+	private MyScreen m_CurrentScreen = MyScreen.SCREEN_TITLE;
+
+	void Update () 
+	{
+		if (Input.anyKeyDown) 
+		{
+			switch (m_CurrentScreen)
+			{
+			case MyScreen.SCREEN_TITLE:
+				bgImage.sprite = introTexture;
+				m_CurrentScreen = MyScreen.SCREEN_INTRO;
+				break;
+			
+			case MyScreen.SCREEN_INTRO:
+				Debug.Log("Load the main level now!");
+				break;
+			}
+		}
+	}
+}
