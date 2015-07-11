362cfd779d4a3f11aee5b2ab39d39ddae7189ab5
RainbowMin <ecust.mj@gmail.com>
2015-07-11 18:13:18 +0800

    相机和物件的鼠标操控

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public Transform _Player;
+	private Vector3 _OffsetPosition;
+	public float _Distance = 0;
+	public bool _IsRotating = false;
+	public float _RotateSpeed = 10;
+	
+	void Start()
+	{
+		//_Player = GameObject.FindGameObjectWithTag (Tags._Player).transform;
+		//transform.LookAt (_Player.position);
+		_OffsetPosition = transform.position - _Player.position;
+	}
+	
+	void Update()
+	{
+		//transform.position = _Player.position + _OffsetPosition;
+		
+		RotateView ();
+	}
+
+	
+	void RotateView()
+	{
+		//Input.GetAxis("Mouse X");//得到鼠标在水平方向的滑动
+		//Input.GetAxis("Mouse Y");//得到鼠标在垂直方向的滑动
+		
+		if(Input.GetMouseButtonDown(1))
+		{
+			_IsRotating = true;
+		}
+		if(Input.GetMouseButtonUp(1))
+		{
+			_IsRotating = false;
+		}
+		
+		if(_IsRotating)
+		{
+			transform.RotateAround(_Player.position, Vector3.up, Input.GetAxis("Mouse X") * _RotateSpeed);
+			transform.RotateAround(_Player.position, transform.right, Input.GetAxis("Mouse Y") * -_RotateSpeed);
+			_OffsetPosition = transform.position - _Player.position;
+		}
+	}
+}
---
+++ Assets/Scripts/ObjectRotate.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ObjectRotate : MonoBehaviour 
+{
+	public float _WheelSpeed = 10.0f;
+	public float _MaxY = 10;
+	public float _MinY = -10;
+	public Slider _UISlider;
+
+	public bool _IsRotating = false;
+	public float _RotateSpeed = 10;
+
+	public Transform _ObjTrans;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ScrollWheel();
+
+		RotateView();
+	}
+
+	void ScrollWheel()
+	{
+		float wheelValue =  Input.GetAxis("Mouse ScrollWheel");
+		float addedY = transform.position.y + _WheelSpeed * wheelValue;
+		if(wheelValue != 0)
+		{
+			if(addedY > _MaxY)
+			{
+				transform.position = new Vector3(transform.position.x, _MaxY, transform.position.z);
+			}
+			else if(addedY < _MinY)
+			{
+				transform.position = new Vector3(transform.position.x, _MinY, transform.position.z);
+			}
+			else
+			{
+				transform.position = new Vector3(transform.position.x, addedY, transform.position.z);
+			}
+		}
+
+		float curY = transform.position.y;
+		_UISlider.value = (_MaxY - curY) / (_MaxY - _MinY);
+	}
+
+	void RotateView()
+	{
+		//Input.GetAxis("Mouse X");//得到鼠标在水平方向的滑动
+		//Input.GetAxis("Mouse Y");//得到鼠标在垂直方向的滑动
+		
+		if(Input.GetMouseButtonDown(0))
+		{
+			_IsRotating = true;
+		}
+		if(Input.GetMouseButtonUp(0))
+		{
+			_IsRotating = false;
+		}
+		
+		if(_IsRotating)
+		{
+			transform.Rotate(Input.GetAxis("Mouse X") * _RotateSpeed, 0,  Input.GetAxis("Mouse Y") * _RotateSpeed);
+		}
+
+	}
+
+}
